fca7c6cdb99d4892ccc22ecbd2d1379e7657e4cc
Joel Pablo <joel.pablo@digitalmonkstudio.com>
2014-06-18 05:39:46 +0800

    package update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// var favicon = require('static-favicon');
+ var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
 
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
@@ -12,14 +12,16 @@ var bcrypt = require('bcryptjs');
 var Show = require('./models/show');
 var User = require('./models/user');
 
+mongoose.connect('localhost');
+
 var app = express();
 
 app.set('port', process.env.PORT || 3001);
-app.use(favicon());
+//app.use(favicon());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded());
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function (){
@@ -47,6 +49,5 @@ app.use(function( err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
